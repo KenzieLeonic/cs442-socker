6335843761890c7b67a78f94bd079cec1c7081a3
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2022-09-27 16:54:58 +0700

    add socket.io

--- app/index.js
+++ app/index.js
@@ -1,24 +1,42 @@
 const express = require('express')
 const PORT = 4000
-const HOST = '0.0.0.0'
+const HOST = '0.0.0.0' //รายการของตัว code
 
-const app = express()
+const app = express() //สร้าง express
 const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server)
+const server = http.createServer() //สร้าง server ส่ง app (express())
+const { Server } = require('socket.io') //เรียก socket server
+const io = new Server(server,{
+    cors:{
+        origin: ['http://localhost:3000']
+    }
+})
+ // Server = class ของ io แล้วส่ง express ไป
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.send('<h1>Hello World</h1>') // send request
 })
 
-server.listen(PORT, HOST)
+server.listen(PORT, HOST) //listen ที่ PORT และ HOST (localhost)
 console.log(`App running on http://${HOST}:${PORT}`)
 
+//SOCKET PART
+// on the connection แล้วสร้าง socket ขึ้นมา
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    console.log(`a user connected: ${socket.id}`)
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+    socket.on('hello.message', (data) => {
+        console.log(`${socket.id} = ${data.sender_id}: ${data.message}`);
+        socket.broadcast.emit('hello.reply',{
+            sender_id: data.sender_id,
+            message: data.message
+        })
+    })
+    socket.on("rewards.create", (data) => {
+        if (data.success) {
+            socket.broadcast.emit('rewards.index', {
+                refresh: true
+            })
+        }
+    })
 })
